dbfe5bb25f7a9d7161f324178f7a7eab6de8902f
Vlad <vsrabotabus@gmail.com>
2026-07-22 13:50:54 +0000

    Initial commit: first bash scripts and lab files

---
+++ sys_info.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "The beginning of the script"
+date
+echo "The beginning of the task"
+cat hardware_info.txt
+echo "The end"
+
---
+++ user_info.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ROLE="DevOps Engineer"
+echo "Hello,$1"
+echo "Your role: $ROLE"
+date

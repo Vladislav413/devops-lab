c5a89e2e9968d48da048e2e24e99af319e9ac4ec
Vlad <vsrabotabus@gmail.com>
2026-07-26 16:30:14 +0000

    Fix: changed relative path to absolute path for cron

--- check_disk.sh
+++ check_disk.sh
@@ -4,9 +4,9 @@ USAGE=$(df / | awk 'NR==2 {print $5}' | tr -d '%')
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ "$USAGE" -gt 80 ]; then
-    echo "[$TIMESTAMP] [WARN] Диск забит на $USAGE%!" >> disk.log
+    echo "[$TIMESTAMP] [WARN] Диск забит на $USAGE%!" >> /root/workspace/disk.log
     echo "[$TIMESTAMP] [WARN] Диск забит на $USAGE%!"
 else
-    echo "[$TIMESTAMP] [OK] Диск в норме: $USAGE%" >> disk.log
+    echo "[$TIMESTAMP] [OK] Диск в норме: $USAGE%" >> /root/workspace/disk.log
     echo "[$TIMESTAMP] [OK] Диск в норме: $USAGE%"
 fi
